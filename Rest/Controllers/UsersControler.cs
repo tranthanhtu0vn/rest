540e1547ca4898c15a9847f250ddaa6cd43416f1
MyERP <tu.tran@yahoo.com>
2018-09-17 22:19:37 +0700

    get users

---
+++ Rest/Controllers/UsersControler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Web.Http;
+
+namespace Rest.Controllers
+{
+    [RoutePrefix("api/users")]
+    public class UsersController: ApiController
+    {
+        public IList<User> users;
+        public UsersController()
+        {
+            this.users = new List<User>();
+            this.users.Add(new User(1,"Tu", "Tran"));
+            this.users.Add(new User(2, "Tu 2", "Tran"));
+        }
+        [Route("")]
+        [HttpGet()]
+        public IList<User> GetUsers() {
+            return this.users;
+        }
+    }
+    [DataContract(Name="User")]
+    public class User {
+        [DataMember(Name="Id")]
+        public int Id { get; set; }
+        [DataMember(Name = "LastName")]
+        public string LastName { get; set; }
+        [DataMember(Name = "FirstName")]
+        public string FirstName { get; set; }
+        public User(int id, string fName, string lName)
+        {
+            this.Id = id;
+            this.FirstName = fName;
+            this.LastName = lName;
+        }
+
+    }
+}
